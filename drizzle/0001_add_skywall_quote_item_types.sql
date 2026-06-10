70569edb3a1e62f97b00d0aa303be6d1285a5adb
maanmanpreet-hash <maan.manpreet@gmail.com>
2026-06-11 09:30:05 +1000

    Add migration for new quote item types

---
+++ drizzle/0001_add_skywall_quote_item_types.sql
@@ -0,0 +1,12 @@
+ALTER TABLE `job_items`
+  MODIFY COLUMN `item_type` enum(
+    'cladding',
+    'acoustic_panel',
+    'floating_cabinet',
+    'fireplace',
+    'mirror',
+    'marble_sheet',
+    'tv_backdrop',
+    'side_tower',
+    'shelving'
+  ) NOT NULL;
